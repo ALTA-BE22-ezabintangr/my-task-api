2b2ccb4fa1de29a2c27b67617be0296abef57d4e
Eza <ezabintangramadhan@gmail.com>
2024-05-05 23:02:48 +0700

    first config

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"myTaskApp/app/configs"
+	"myTaskApp/app/databases"
+	"myTaskApp/app/routes"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	cfg := configs.InitConfig()
+	dbMySql := databases.InitDBMysql(cfg)
+	// dbMysql := databases.InitDBPosgres(cfg)
+
+	// create new instance echo
+	e := echo.New()
+
+	// migrations.InitialMigration()
+	routes.InitRouter(e, dbMySql)
+
+	// start server and port
+	e.Logger.Fatal(e.Start(":8080"))
+}
